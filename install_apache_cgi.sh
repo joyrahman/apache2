25ba4821a95842cd9e54eba604c437c73d1d23f7
joyrahman <joyrahman@gmail.com>
2015-01-15 17:14:20 -0600

    Create install_apache_cgi.sh

---
+++ install_apache_cgi.sh
@@ -0,0 +1,15 @@
+sudo apt-get install apache2 collectd git -y
+sudo apt-get install librrds-perl libconfig-general-perl libhtml-parser-perl  libregexp-common-perl
+sudo /etc/init.d/collectd restart
+
+
+git clone https://github.com/joyrahman/apache2.git
+cd apache2
+sudo cp -rf www/ /var/www
+sudo cp -rf sites-available/ sites-enabled/ /etc/apache2
+sudo cp -rf apache2.conf httpd.conf /etc/apache2/
+
+chmod a+x /var/www/collection3/bin/*.cgi
+
+sudo /etc/init.d/apache2 restart
+
